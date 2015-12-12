b1e480dd1008eb7fbbd6bd7dba3c8a37ea42f8d8
Eric Lathrop <eric@ericlathrop.com>
2015-12-12 14:19:33 -0500

    Tile on negative y axis

--- src/systems/renderer/tile-background.js
+++ src/systems/renderer/tile-background.js
@@ -14,10 +14,14 @@ function getScreenTopLeft(camera) {
 }
 
 var time = 0;
+var rowHeight = 60;
+var waveHeight = 20;
+var wavePeriod = 4000;
+var rowsBeforeRepeat = 8;
+var rowsOffset = Math.PI * 2 / rowsBeforeRepeat;
 
 module.exports = function(ecs, data) { // jshint ignore:line
 	ecs.add(function(entities, context, elapsed) { // jshint ignore:line
-
 		var camera = getCamera(data.entities.entities);
 		context.fillStyle = "#1c325f";
 		context.fillRect(Math.floor(camera.position.x), Math.floor(camera.position.y), data.canvas.width, data.canvas.height);
@@ -26,23 +30,19 @@ module.exports = function(ecs, data) { // jshint ignore:line
 
 		var f1 = data.images.get("waves");
 
-		var rowHeight = 60;
 		var screen = getScreenTopLeft(camera);
 		var startX = Math.floor(screen.x / f1.width) * f1.width;
 		var startRow = Math.floor(screen.y / rowHeight);
-		var startY = startRow - 1 * rowHeight;
-
-		var waveHeight = 20;
-		var wavePeriod = 3000;
-		var rowsBeforeRepeat = 8;
-		var rowsOffset = Math.PI * 2 / rowsBeforeRepeat;
+		var startY = (startRow - 1) * rowHeight;
 
 		for (var y = startY; y <= screen.y + data.canvas.height; y += rowHeight) {
 			var even = Math.floor(y / rowHeight) % 2;
 			var offset = y / rowHeight % rowsBeforeRepeat * rowsOffset;
-			var wavedY = y + Math.sin(time / wavePeriod * Math.PI * 2 + offset) * waveHeight;
-			for (var x = startX; x <= screen.x + data.canvas.width; x += f1.width) {
-				context.drawImage(f1, even ? x : x - 100, wavedY, f1.width, f1.height);
+			var waveY = y + Math.sin(time / wavePeriod * Math.PI * 2 + offset) * waveHeight;
+
+			for (var x = startX; x <= screen.x + data.canvas.width + f1.width; x += f1.width) {
+				var waveX = x + Math.sin(time / wavePeriod * 2 * Math.PI * 2 + offset) * waveHeight / 2;
+				context.drawImage(f1, even ? waveX : waveX - 100, waveY, f1.width, f1.height);
 			}
 		}
 	});
