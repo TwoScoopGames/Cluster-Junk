2d89c8f4d94890bd8b52d06c65d93d8982b1498a
Eric Lathrop <eric@ericlathrop.com>
2015-12-12 16:44:09 -0500

    Add trash collection

--- src/systems/simulation/handle-collisions.js
+++ src/systems/simulation/handle-collisions.js
@@ -1,10 +1,26 @@
 "use strict";
 
+var onEntityDelete = require("splat-ecs/lib/systems/box-collider").onEntityDelete;
+
 module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
-	ecs.addEach(function(player, elapsed) { // eslint-disable-line no-unused-vars
-		if (player.collisions.length === 0) {
+	ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
+		if (entity.collisions.length === 0) {
 			return;
 		}
-		// console.log(player.collisions);
-	}, ["player"]);
+		entity.collisions.forEach(function(id) {
+			var other = data.entities.entities[id];
+			if (other.sticky) {
+				return;
+			}
+
+			other.match = {
+				id: entity.id,
+				offsetX: other.position.x - entity.position.x,
+				offsetY: other.position.y - entity.position.y
+			};
+			other.sticky = true;
+			other.velocity = { x: 0, y: 0 };
+			onEntityDelete(other, data);
+		});
+	}, ["sticky"]);
 };
