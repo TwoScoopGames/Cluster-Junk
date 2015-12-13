9a28cd35e8a8dcad47aef2d3a647d4a1fc2f59c1
Eric Lathrop <eric@ericlathrop.com>
2015-12-12 19:19:23 -0500

    Zoom out on growth

--- src/systems/simulation/handle-collisions.js
+++ src/systems/simulation/handle-collisions.js
@@ -2,6 +2,10 @@
 
 var onEntityDelete = require("splat-ecs/lib/systems/box-collider").onEntityDelete;
 
+function getCamera(entities) {
+	return entities[1];
+}
+
 module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
 		if (entity.collisions.length === 0) {
@@ -13,6 +17,9 @@ module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 				return;
 			}
 
+			data.canvas.width += 50;
+			data.canvas.height += 50;
+
 			other.match = {
 				id: entity.id,
 				offsetX: other.position.x - entity.position.x,
@@ -22,5 +29,8 @@ module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 			other.velocity = { x: 0, y: 0 };
 			onEntityDelete(other, data);
 		});
+		var camera = getCamera(data.entities.entities);
+		camera.size.width = data.canvas.width;
+		camera.size.height = data.canvas.height;
 	}, ["sticky"]);
 };
