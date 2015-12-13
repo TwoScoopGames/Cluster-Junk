8314dbf819c976b304a7161f014ba1cbeea2d074
Alex Bezuska <abezuska@gmail.com>
2015-12-13 17:59:08 -0500

    add new goal colors

--- src/systems/renderer/goal.js
+++ src/systems/renderer/goal.js
@@ -9,12 +9,12 @@ module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 
 		var progressRadius = (entity.radius / entity.goalRadius) * radius;
 
-		context.fillStyle = "green";
+		context.fillStyle = "rgba(106, 189, 68, 0.55)";
 		context.beginPath();
 		context.arc(cx, cy, progressRadius, 0, Math.PI * 2);
 		context.fill();
 
-		context.strokeStyle = "yellow";
+		context.strokeStyle = "rgba(228, 227, 158, 0.48)";
 		context.beginPath();
 		context.arc(cx, cy, radius, 0, Math.PI * 2);
 		context.stroke();
