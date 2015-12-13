0b1869d002dba75e65eff5023329ac894c8964f4
Eric Lathrop <eric@ericlathrop.com>
2015-12-13 17:46:08 -0500

    Fix radar line

--- src/systems/renderer/radar.js
+++ src/systems/renderer/radar.js
@@ -2,7 +2,6 @@
 
 var simpleEasing = require("../../simpleEasing");
 
-
 function drawLine(context, ray, width, color) {
 	context.strokeStyle = color;
 	context.lineWidth = width;
@@ -11,31 +10,36 @@ function drawLine(context, ray, width, color) {
 	context.lineTo(ray.endX, ray.endY);
 	context.stroke();
 }
+
 var angle = 0;
 
 var radarY = -50;
-var lineStartY = radarY - 80;
+
+var period = 5000;
+var radius = 80;
 
 module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 	ecs.addEach(function(player, context, elapsed) { // eslint-disable-line no-unused-vars
 
 		radarY += simpleEasing(radarY, 5, 0.2);
+
 		var radarImage = data.images.get("radar");
-		context.drawImage(radarImage, 5,radarY);
-		var period = 5000;
-		var radius = 80;
-		var angularVelocity = -((Math.PI * 2)/ period);
+		context.drawImage(radarImage, 5, radarY);
+
+		var angularVelocity = -((Math.PI * 2) / period);
 		var distanceThisFrame = angularVelocity * elapsed;
 		angle += distanceThisFrame;
+
+		var cx = 5 + Math.floor(radarImage.width / 2);
+		var cy = radarY + Math.floor(radarImage.height / 2);
 		var x = radius * Math.cos(angle);
 		var y = radius * Math.sin(angle);
 
-		lineStartY += simpleEasing(lineStartY, 85, 0.2);
 		var linePoints = {
-			"startX": 85,
-			"startY":lineStartY,
-			"endX":85 + x,
-			"endY":85 + y
+			"startX": cx,
+			"startY": cy,
+			"endX": cx + x,
+			"endY": cy + y
 		};
 		drawLine(context, linePoints, "1px", "#6abd44");
 	}, ["player"]);
