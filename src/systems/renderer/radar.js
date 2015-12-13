b2456296407f0219601c2025137453d6688af57a
Eric Lathrop <eric@ericlathrop.com>
2015-12-13 18:23:06 -0500

    Draw trash on radar

--- src/systems/renderer/radar.js
+++ src/systems/renderer/radar.js
@@ -42,5 +42,29 @@ module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
 			"endY": cy + y
 		};
 		drawLine(context, linePoints, "1px", "#6abd44");
+
+		Object.keys(data.entities.entities).forEach(function(id) {
+			var entity = data.entities.entities[id];
+			if (entity.type !== "trash" || entity.sticky || entity.player) {
+				return;
+			}
+
+			var dx = entity.position.x - player.position.x;
+			var dy = entity.position.y - player.position.y;
+
+			var max = 1000 * 1000;
+			var dist = Math.max(Math.min((dx * dx + dy * dy), max), -max);
+			var scaledDist = dist / max * (radius - 3);
+
+			var angle = Math.atan2(entity.position.y - player.position.y, entity.position.x - player.position.x);
+
+			var ex = scaledDist * Math.cos(angle);
+			var ey = scaledDist * Math.sin(angle);
+
+			context.strokeStyle = "#6abd44";
+			context.beginPath();
+			context.arc(cx + ex, cy + ey, 3, 0, Math.PI * 2);
+			context.stroke();
+		});
 	}, ["player"]);
 };
