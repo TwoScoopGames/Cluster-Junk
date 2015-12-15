a56bc531c5ebf575356ecf59bf0add5871d1d5eb
Alex Bezuska <abezuska@gmail.com>
2015-12-14 19:42:22 -0500

    add easing functions

--- src/systems/renderer/draw-notice.js
+++ src/systems/renderer/draw-notice.js
@@ -1,9 +1,38 @@
 "use strict";
 
-var simpleEasing = require("../../simpleEasing");
+var easing = require("../../easing");
 
-var noticeY = -300;
-var noticeTextY = noticeY + 168;
+var noticeStart = -300;
+
+var time = 0;
+var duration = 2000;
+
+module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
+	ecs.addEach(function(notice, context, elapsed) { // eslint-disable-line no-unused-vars
+		if(notice.message){
+
+			//noticeboard
+			var noticeY = -60;
+			if(time < duration){
+				time += elapsed;
+				noticeY = easing.bounceEaseOut(time, noticeStart, 230, duration);
+			}
+			var noticeBoard = data.images.get("notice");
+			context.drawImage(noticeBoard, (data.canvas.width / 2) - (noticeBoard.width /2), noticeY);
+
+
+
+			if(time >= duration){
+				context.fillStyle = "#3e311a";
+				context.font = "55px blanch";
+				centerText(data.canvas, context, notice.message, 0, 130);
+			}
+
+		}
+	}, ["notice"]);
+};
+
+
 
 function centerText(canvas, context, text, offsetX, offsetY) {
 	var w = context.measureText(text).width;
@@ -11,19 +40,3 @@ function centerText(canvas, context, text, offsetX, offsetY) {
 	var y = offsetY | 0;
 	context.fillText(text, x, y);
 }
-
-
-module.exports = function(ecs, data) { // eslint-disable-line no-unused-vars
-	ecs.addEach(function(notice, context) { // eslint-disable-line no-unused-vars
-		if(notice.message){
-			noticeY += simpleEasing(noticeY, -80, 0.2);
-			var noticeBoard = data.images.get("notice");
-			context.drawImage(noticeBoard, (data.canvas.width / 2) - (noticeBoard.width /2), noticeY);
-
-			context.fillStyle = "#3e311a";
-			context.font = "55px blanch";
-			noticeTextY += simpleEasing(noticeTextY, 106, 0.2);
-			centerText(data.canvas, context, notice.message, 0, noticeTextY);
-		}
-	}, ["notice"]);
-};
