e4bf0d2f951a3622e13bbb040d73c225c2eaf282
Eric Lathrop <eric@ericlathrop.com>
2016-07-18 20:01:03 -0400

    Moving waves on title screen

--- src/systems/renderer/title-screen.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
-  ecs.addEach(function renderTitleScreen(entity, elapsed) { // eslint-disable-line no-unused-vars
-    var titleScreen = game.images.get("titleScreen.png");
-    game.context.drawImage(titleScreen, 0, 0);
-  }, "title");
-};
