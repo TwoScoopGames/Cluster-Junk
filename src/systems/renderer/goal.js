50be22e8742549656cb090000166531fcb72d367
Ben <therealbenwiley@gmail.com>
2016-07-26 22:11:16 -0400

    Goal progress text displayed in circle.

--- src/systems/renderer/goal.js
+++ src/systems/renderer/goal.js
@@ -22,5 +22,28 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
     game.context.arc(cx, cy, radius, 0, Math.PI * 2);
     game.context.stroke();
 
+    var progressText = Math.floor(playerRadius);
+    var goalText = "/" + goalRadius + "m";
+    game.context.fillStyle = "#ffffff";
+    game.context.strokeStyle = "#3e311a";
+
+    var camera = game.entities.find("camera")[0];
+    var cameraHeight = game.entities.get(camera, "size").height;
+    var scale = game.canvas.height / cameraHeight;
+
+    game.context.font = "68px blanch";
+    game.context.lineWidth = 8;
+    var progressTextX = cx - (game.context.measureText(progressText).width / 2) | 0;
+    var progressTextY = cy;
+    game.context.strokeText(progressText, progressTextX, progressTextY);
+    game.context.fillText(progressText, progressTextX, progressTextY);
+
+    game.context.font = "30px blanch";
+    game.context.lineWidth = 4;
+    var goalTextX = cx + 25 - (game.context.measureText(goalText).width / 2) | 0;
+    var goalTextY = cy + 30;
+    game.context.strokeText(goalText, goalTextX, goalTextY);
+    game.context.fillText(goalText, goalTextX, goalTextY);
+
   }, ["player"]);
 };
