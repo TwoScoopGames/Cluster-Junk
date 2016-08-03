04398b918613a1642540e9095012fade1030c267
Eric Lathrop <eric@ericlathrop.com>
2016-08-02 22:47:08 -0400

    Add delay before moving box

---
+++ src/scripts/move-box-into-water.js
@@ -0,0 +1,41 @@
+
+module.exports = function(entity, game) {
+  var position = game.entities.get(entity, "position");
+
+  var viewport = 0;
+  game.entities.set(viewport, "follow", {
+    "id": entity,
+    "distance": 200
+  });
+  game.entities.get(entity, "timers").playSplashSfx.running = true;
+  game.entities.set(entity, "moveToPoint", {
+    "x": 1500,
+    "y": 1550,
+    "maxDistanceAway": 10,
+    "script": "./scripts/strike-and-transform-box"
+  });
+  game.entities.set(entity, "easing", {
+    "scale.scale": {
+      "type": "easeOutCubic",
+      "start": 2.0,
+      "end": 1.0,
+      "time": 0,
+      "max": 500
+    },
+    "position.x": {
+      "type": "easeOutQuart",
+      "start": position.x,
+      "end": position.x + 500,
+      "time": 0,
+      "max": 3000
+    },
+    "position.y": {
+      "type": "easeOutElastic",
+      "start": position.y,
+      "end": position.y + 500,
+      "time": 0,
+      "max": 3000
+    }
+  });
+  game.entities.remove(entity, "match");
+};
--- src/scripts/strike-box.js
+++ src/scripts/strike-box.js
@@ -2,55 +2,15 @@
 
 var spawnLightning = require("../spawn-lightning");
 
-var lastLeft = false;
 module.exports = function(entity, game) {
   var position = game.entities.get(entity, "position");
   var viewport = 0;
   var viewportPosition = game.entities.get(viewport, "position");
   var viewportSize = game.entities.get(viewport, "size");
-  var x = lastLeft ? viewportPosition.x : viewportPosition.x + viewportSize.width;
-  lastLeft = !lastLeft;
+  var x = viewportPosition.x + viewportSize.width;
   spawnLightning(x, viewportPosition.y, position.x + 50, position.y + 40, game, 0.5);
-  game.sounds.play(lastLeft ? "sfx-lightning-1.mp3" : "sfx-lightning-2.mp3");
+  game.sounds.play("sfx-lightning-1.mp3");
 
   game.entities.remove(entity, "onPositionLeftOf");
-  if (position.x < 500) {
-    game.entities.set(viewport, "follow", {
-      "id": entity,
-      "distance": 200
-    });
-    game.entities.get(entity, "timers").playSplashSfx.running = true;
-    game.entities.set(entity, "moveToPoint", {
-      "x": 1500,
-      "y": 1550,
-      "maxDistanceAway": 10,
-      "script": "./scripts/strike-and-transform-box"
-    });
-    game.entities.set(entity, "easing", {
-      "scale.scale": {
-        "type": "easeOutCubic",
-        "start": 2.0,
-        "end": 1.0,
-        "time": 0,
-        "max": 500
-      },
-      "position.x": {
-        "type": "easeOutQuart",
-        "start": position.x,
-        "end": position.x + 500,
-        "time": 0,
-        "max": 3000
-      },
-      "position.y": {
-        "type": "easeOutElastic",
-        "start": position.y,
-        "end": position.y + 500,
-        "time": 0,
-        "max": 3000
-      }
-    });
-    game.entities.remove(entity, "match");
-  } else {
-    game.entities.set(entity, "onPositionLeftOf", { x: position.x - 100, script: "./scripts/strike-box" });
-  }
+  game.entities.get(entity, "timers").moveBoxIntoWater.running = true;
 };
