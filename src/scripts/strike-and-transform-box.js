21a757ce755b4347cb9a380af841aa57f6b8ec40
Eric Lathrop <eric@ericlathrop.com>
2016-08-05 22:33:39 -0400

    Add delay after second lightning strike

--- src/scripts/strike-and-transform-box.js
+++ src/scripts/strike-and-transform-box.js
@@ -4,7 +4,6 @@ var spawnLightning = require("../spawn-lightning");
 
 module.exports = function(entity, game) {
   var position = game.entities.get(entity, "position");
-  var viewport = 0;
   var camera = 1;
   var cameraPosition = game.entities.get(camera, "position");
   spawnLightning(cameraPosition.x, cameraPosition.y, position.x + 50, position.y, game, 0.5);
@@ -19,22 +18,4 @@ module.exports = function(entity, game) {
     "speed": 1
   });
   game.entities.get(entity, "timers").switchAnims.running = true;
-  game.entities.get(viewport, "follow").distance = 0;
-  game.entities.remove(camera, "matchCanvasSize");
-  game.entities.set(camera, "easing", {
-    "size.width": {
-      "type": "easeInOutElastic",
-      "start": 1136,
-      "end": 400,
-      "time": 0,
-      "max": 1000
-    },
-    "size.height": {
-      "type": "easeInOutElastic",
-      "start": 640,
-      "end": 225,
-      "time": 0,
-      "max": 1000
-    }
-  });
 };
--- src/scripts/switch-box-anims.js
+++ src/scripts/switch-box-anims.js
@@ -1,6 +1,27 @@
 "use strict";
 
 module.exports = function(entity, game) {
+  var viewport = 0;
+  var camera = 1;
+  game.entities.get(viewport, "follow").distance = 0;
+  game.entities.remove(camera, "matchCanvasSize");
+  game.entities.set(camera, "easing", {
+    "size.width": {
+      "type": "easeInOutElastic",
+      "start": 1136,
+      "end": 400,
+      "time": 0,
+      "max": 1000
+    },
+    "size.height": {
+      "type": "easeInOutElastic",
+      "start": 640,
+      "end": 225,
+      "time": 0,
+      "max": 1000
+    }
+  });
+
   game.entities.get(entity, "animation").name = "box-anim-f11";
   game.entities.set(entity, "eyes", {
     "pupilOffsetX": 0,
