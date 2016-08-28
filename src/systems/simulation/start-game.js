d06aa78b7044013ecc6b0a1002f1ea4756746f03
Eric Lathrop <eric@ericlathrop.com>
2016-08-27 20:56:19 -0400

    Convert system to component

--- src/systems/simulation/start-game.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
-  ecs.addEach(function startGame(entity, elapsed) { // eslint-disable-line no-unused-vars
-    if (game.inputs.buttonPressed("action")) {
-      game.switchScene("level");
-    }
-  }, "title");
-};
