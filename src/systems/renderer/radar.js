2585d9539d44f3921e177738315505e61e366c75
Eric Lathrop <eric@ericlathrop.com>
2016-09-09 00:42:48 -0400

    Create less functions and objects

--- src/systems/renderer/radar.js
+++ src/systems/renderer/radar.js
@@ -25,64 +25,57 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
     game.context.drawImage(radarImage, 5, radarY);
 
     // radar screen
-    if (time >= duration) {
+    if (time < duration) {
+      return;
+    }
 
-      var angularVelocity = -((Math.PI * 2) / period);
-      var distanceThisFrame = angularVelocity * elapsed;
-      angle += distanceThisFrame;
+    var angularVelocity = -((Math.PI * 2) / period);
+    var distanceThisFrame = angularVelocity * elapsed;
+    angle += distanceThisFrame;
 
-      var cx = 5 + Math.floor(radarImage.width / 2);
-      var cy = radarY + Math.floor(radarImage.height / 2);
-      var x = radius * Math.cos(angle);
-      var y = radius * Math.sin(angle);
+    var cx = 5 + Math.floor(radarImage.width / 2);
+    var cy = radarY + Math.floor(radarImage.height / 2);
+    var x = radius * Math.cos(angle);
+    var y = radius * Math.sin(angle);
 
-      var linePoints = {
-        "startX": cx,
-        "startY": cy,
-        "endX": cx + x,
-        "endY": cy + y
-      };
-      drawLine(game.context, linePoints, 1, "#6abd44");
+    game.context.strokeStyle =  "#6abd44";
+    game.context.lineWidth = 1;
+    game.context.beginPath();
+    game.context.moveTo(cx, cy);
+    game.context.lineTo(cx + x, cy + y);
+    game.context.stroke();
 
-      var playerPosition = game.entities.getComponent(entity, "position");
+    var playerPosition = game.entities.getComponent(entity, "position");
 
-      game.entities.find("position").forEach(function(id) {
-        var entityType = game.entities.getComponent(id, "type");
-        var entitySticky = game.entities.getComponent(id, "sticky");
-        var entityPlayer = game.entities.getComponent(id, "player");
-        var entityPosition = game.entities.getComponent(id, "position");
+    var ids = game.entities.find("position");
+    for (var i = 0; i < ids.length; i++) {
+      var id = ids[i];
+      var entityType = game.entities.getComponent(id, "type");
+      var entitySticky = game.entities.getComponent(id, "sticky");
+      var entityPlayer = game.entities.getComponent(id, "player");
+      var entityPosition = game.entities.getComponent(id, "position");
 
-        if ((entityType !== "trash" && entityType !== "obstacle") || entitySticky || entityPlayer) {
-          return;
-        }
+      if ((entityType !== "trash" && entityType !== "obstacle") || entitySticky || entityPlayer) {
+        continue;
+      }
 
-        var dx = entityPosition.x - playerPosition.x;
-        var dy = entityPosition.y - playerPosition.y;
+      var dx = entityPosition.x - playerPosition.x;
+      var dy = entityPosition.y - playerPosition.y;
 
-        var max = 1000 * 1000;
-        var dist = Math.max(Math.min((dx * dx + dy * dy), max), -max);
-        var scaledDist = dist / max * (radius - 3);
+      var max = 1000 * 1000;
+      var dist = Math.max(Math.min((dx * dx + dy * dy), max), -max);
+      var scaledDist = dist / max * (radius - 3);
 
-        var angle = Math.atan2(entityPosition.y - playerPosition.y, entityPosition.x - playerPosition.x);
+      var boxAngle = Math.atan2(entityPosition.y - playerPosition.y, entityPosition.x - playerPosition.x);
 
-        var ex = scaledDist * Math.cos(angle);
-        var ey = scaledDist * Math.sin(angle);
+      var ex = scaledDist * Math.cos(boxAngle);
+      var ey = scaledDist * Math.sin(boxAngle);
 
-        game.context.fillStyle = "#6abd44";
-        if (entityType === "obstacle") {
-          game.context.fillStyle = "red";
-        }
-        game.context.fillRect(cx + ex - 1, cy + ey - 1, 3, 3);
-      });
+      game.context.fillStyle = "#6abd44";
+      if (entityType === "obstacle") {
+        game.context.fillStyle = "red";
+      }
+      game.context.fillRect(Math.floor(cx + ex - 1), Math.floor(cy + ey - 1), 3, 3);
     }
   }, "player");
 };
-
-function drawLine(context, ray, width, color) {
-  context.strokeStyle = color;
-  context.lineWidth = width;
-  context.beginPath();
-  context.moveTo(ray.startX, ray.startY);
-  context.lineTo(ray.endX, ray.endY);
-  context.stroke();
-}
