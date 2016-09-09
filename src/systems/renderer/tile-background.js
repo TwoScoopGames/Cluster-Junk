e7d70beb3293111e0b04c9f2f8113c1509708021
Eric Lathrop <eric@ericlathrop.com>
2016-09-08 21:51:04 -0400

    Make tile-background system faster

--- src/systems/renderer/tile-background.js
+++ src/systems/renderer/tile-background.js
@@ -6,15 +6,13 @@ var waveHeight = 20;
 var wavePeriod = 4000;
 var rowsBeforeRepeat = 8;
 var rowsOffset = Math.PI * 2 / rowsBeforeRepeat;
+var sinPeriod = 2 * Math.PI;
 
 module.exports = function(ecs, game) {
   ecs.add(function tileBackground(entities, elapsed) {
     var camera = 1;
     var cameraPosition = game.entities.getComponent(camera, "position");
     var cameraSize = game.entities.getComponent(camera, "size");
-
-    game.context.fillStyle = "#1c325f";
-    game.context.fillRect(Math.floor(cameraPosition.x), Math.floor(cameraPosition.y), cameraSize.width, cameraSize.height);
 
     time += elapsed;
 
@@ -24,14 +22,25 @@ module.exports = function(ecs, game) {
     var startRow = Math.floor(cameraPosition.y / rowHeight);
     var startY = (startRow - 1) * rowHeight;
 
-    for (var y = startY; y <= cameraPosition.y + cameraSize.height; y += rowHeight) {
-      var even = Math.floor(y / rowHeight) % 2;
-      var offset = y / rowHeight % rowsBeforeRepeat * rowsOffset;
-      var waveY = y + Math.sin(time / wavePeriod * Math.PI * 2 + offset) * waveHeight;
+    var periodPercent = time / wavePeriod;
+    var something = periodPercent * sinPeriod;
 
-      for (var x = startX; x <= cameraPosition.x + cameraSize.width + f1.width; x += f1.width) {
-        var waveX = x + Math.sin(time / wavePeriod * 2 * Math.PI * 2 + offset) * waveHeight / 2;
-        game.context.drawImage(f1, even ? waveX : waveX - 100, waveY, f1.width, f1.height);
+    var cameraRight = cameraPosition.x + cameraSize.width + f1.width;
+    var cameraBottom = cameraPosition.y + cameraSize.height;
+
+    for (var y = startY; y <= cameraBottom; y += rowHeight) {
+
+      var offset = y / rowHeight % rowsBeforeRepeat * rowsOffset;
+
+      var waveY = Math.floor(y + Math.sin(something + offset) * waveHeight);
+
+      var rowX = Math.sin(something * 2 + offset) * waveHeight / 2;
+      var even = Math.floor(y / rowHeight) % 2;
+      rowX = even ? rowX : rowX - 100;
+
+      for (var x = startX; x <= cameraRight; x += f1.width) {
+        var waveX = Math.floor(x + rowX);
+        game.context.drawImage(f1, waveX, waveY, f1.width, f1.height);
       }
     }
   });
