6085bfe28fd2ec7fe73c44fc1bb0139aa3278114
Ben Wiley <therealbenwiley@gmail.com>
2016-09-19 23:45:00 -0400

    barge fog horn thing stops when we want it to.

--- src/scripts/intro-exit.js
+++ src/scripts/intro-exit.js
@@ -4,4 +4,5 @@ module.exports = function(game) { // eslint-disable-line no-unused-vars
   game.sounds.stop("trash-island-thunderstorm.mp3");
   game.sounds.stop("trash-island-storm-intro-a.mp3");
   game.sounds.stop("trash-island-storm-intro-b.mp3");
+  game.sounds.stop("sfx-fog-horn.mp3");
 };
--- src/scripts/play-fog-horn.js
+++ src/scripts/play-fog-horn.js
@@ -1,4 +1,7 @@
 
 module.exports = function(entity, game) {
-  game.sounds.play("sfx-fog-horn.mp3");
+  game.sounds.play("sfx-fog-horn.mp3", {
+    "loopStart": 2.895,
+    "loopEnd": 2.901
+  });
 };
