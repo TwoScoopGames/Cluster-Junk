6ac78d21269273ab30d5b7d216dbb83140f15cf8
Eric Lathrop <eric@ericlathrop.com>
2016-10-02 22:26:31 -0400

    Make analog movement behave more like digital one

--- src/components/movement-2d-analog.js
+++ src/components/movement-2d-analog.js
@@ -1,12 +1,14 @@
 module.exports = {
   factory: function() {
     return {
+      accel: 0.02,
       angle: 0,
       magnitude: 0,
       max: 1
     };
   },
   reset: function(movement2dAnalog) {
+    movement2dAnalog.accel = 0.02;
     movement2dAnalog.angle = 0;
     movement2dAnalog.magnitude = 0;
     movement2dAnalog.max = 1.0;
--- src/systems/simulation/apply-movement-2d-analog.js
+++ src/systems/simulation/apply-movement-2d-analog.js
@@ -1,14 +1,17 @@
 module.exports = function(ecs, game) {
   game.entities.registerSearch("applyMovement2dAnalog", ["velocity", "movement2dAnalog"]);
-  // game.entities.registerSearch("applyMovement2dAnalog", ["acceleration", "movement2dAnalog"]);
   ecs.addEach(function applyMovement2dAnalog(entity) {
     var velocity = game.entities.getComponent(entity, "velocity");
-    // var acceleration = game.entities.getComponent(entity, "acceleration");
     var movement2dAnalog = game.entities.getComponent(entity, "movement2dAnalog");
 
-    velocity.x = movement2dAnalog.magnitude * movement2dAnalog.max * Math.cos(movement2dAnalog.angle);
-    velocity.y = movement2dAnalog.magnitude * movement2dAnalog.max * Math.sin(movement2dAnalog.angle);
-    // acceleration.x = movement2dAnalog.magnitude * movement2dAnalog.max * Math.cos(movement2dAnalog.angle);
-    // acceleration.y = movement2dAnalog.magnitude * movement2dAnalog.max * Math.sin(movement2dAnalog.angle);
+    velocity.x += movement2dAnalog.magnitude * movement2dAnalog.accel * Math.cos(movement2dAnalog.angle);
+    velocity.y += movement2dAnalog.magnitude * movement2dAnalog.accel * Math.sin(movement2dAnalog.angle);
+
+    var magnitude = Math.sqrt((velocity.x * velocity.x) + (velocity.y + velocity.y));
+    if (magnitude > movement2dAnalog.max) {
+      var angle = Math.atan2(velocity.y, velocity.x);
+      velocity.x = movement2dAnalog.max * Math.cos(angle);
+      velocity.y = movement2dAnalog.max * Math.sin(angle);
+    }
   }, "applyMovement2dAnalog");
 };
